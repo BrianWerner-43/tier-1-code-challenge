4db256e4d195282e726d7f6f78f417a1f57cf384
Brian Werner <brianmw199@gmail.com>
2023-05-02 18:14:49 -0500

    pushed the odjects into the empty array removed the last item in our array created a for loop to get the sum of all the scores created a variable with the value of myScore checked to see weather or not myScore is higher than highScore

--- client.js
+++ client.js
@@ -9,10 +9,15 @@ console.log('Step 0 complete! client.js loaded!');
             
 // Write your code here!
 
+
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
+
 let highScore = 29;
+
 // 2. Make a new empty array called myScrabbleTiles.
+
  let myScrabbleTiles = [];
+
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
   { tile: 'N', score: 1 }
@@ -22,47 +27,53 @@ let highScore = 29;
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
-let tiles = {
-  tile: 'N', score: 1,
-  tile: 'K', score: 5,
-  tile: 'Z', score: 10,
-  tile: 'X', score: 8,
-  tile: 'D', score: 2,
-  tile: 'A', score: 1
-}
-myScrabbleTiles.push(tiles[6]);
-console.log(tiles);
-  // Push object into myScrabbleTiles array, but only getting the last item in the array
-  
+// pushed the objects into the empty array
+myScrabbleTiles.push({ tile: 'N', score: 1 });
+myScrabbleTiles.push({ tile: 'K', score: 5 });
+myScrabbleTiles.push({ tile: 'Z', score: 10 });
+myScrabbleTiles.push({ tile: 'X', score: 8 } );
+myScrabbleTiles.push({  tile: 'D', score: 2 });
+myScrabbleTiles.push({ tile: 'A', score: 1 });
 
-
+console.log('myScrabbleTiles', 'is', myScrabbleTiles);
 // 4. Remove the last tile from myScrabbleTiles.
-myScrabbleTiles.pop(5);
-console.log(myScrabbleTiles);
+
+myScrabbleTiles.pop();
 
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
-let newTile = {
-  tile: 'F', score: 4
-}
-myScrabbleTiles.push(newTile);
-console.log(myScrabbleTiles);
+
+myScrabbleTiles.push({ tile: 'F', score: 4} );
+
 
 // 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
-function sumTiles(tiles) {
-  for(i=0; myScrabbleTiles.length; i++) {
-    return sumTiles;
+function sumTiles(anArray) {
+  let sum =0; // hold the sum of tile object scores
+
+  // for loop to iterate over anArray an add each score to sum variable
+  for (let i=0; i< anArray.length; i++) {
+   sum = sum + anArray[i].score;
   }
+  console.log('sum is', sum);
+  return sum;
 }
 
+console.log('sumTiles returns:', sumTiles (myScrabbleTiles));
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
-let myScore = 4;
+let myScore = sumTiles(myScrabbleTiles);
 
 // 8. Check whether or not your score is higher than the highScore.
 // 8-1 If your score is higher, change highScore to the new high score.
 
+if ( myScore > highScore) {
+  highScore = myScore;
+}
+// Ternary
+// myScore > highScore ? highScore = myScore :
+
+console.log('highScore is now:', highScore);
 
 // DONE!
